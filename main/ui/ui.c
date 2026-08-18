42ab8bded74c9455e95f62be46ca6ef0a3d65366
xiao.fang <xiao.fang@coolkit.cn>
2026-08-18 10:22:03 +0800

    style: intensify focus alert color

--- main/ui/ui.c
+++ main/ui/ui.c
@@ -21,6 +21,7 @@
 #define COLOR_TEAL     0x5DCAA5
 #define COLOR_GREEN    0x4ADE80
 #define COLOR_RED      0xF87171
+#define COLOR_ALERT_RED 0xFF2D2D
 #define COLOR_ORANGE   0xF7931A
 #define COLOR_SUN      0xF5C453
 #define COLOR_CLOUD    0xF7F8FF
@@ -510,8 +511,8 @@ static void render_focus(lv_obj_t *screen, const navigation_t *navigation) {
     const uint32_t seconds = remaining % 60;
     const bool pomodoro = navigation->focus_duration_seconds == 25 * 60;
     const bool alerting = navigation->focus_alert_until_us != 0;
-    const uint32_t alert_background = navigation->focus_alert_flash_on ? COLOR_RED : COLOR_PRIMARY;
-    const uint32_t alert_foreground = navigation->focus_alert_flash_on ? COLOR_PRIMARY : COLOR_RED;
+    const uint32_t alert_background = navigation->focus_alert_flash_on ? COLOR_ALERT_RED : COLOR_PRIMARY;
+    const uint32_t alert_foreground = navigation->focus_alert_flash_on ? COLOR_PRIMARY : COLOR_ALERT_RED;
     if (alerting) {
         lv_obj_set_style_bg_color(screen, lv_color_hex(alert_background), 0);
         lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
